3f22855ed97d66d74e71ad1c92fa2712960da42d
Peter Levinsky <pele@easj.dk>
2022-09-23 10:40:32 +0200

    Add project files.

---
+++ ClassDemoStuderendeKlasse/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassDemoStuderendeKlasse;
+
+
+
+Studerende studerende1 = new Studerende(); // laver objekt + kalder konstruktør
+            // opretter en reference til et nyt objekt af klassen Studerende
+UdskrivStuderende(studerende1); // udskriv 1
+
+studerende1.StudieId = "DAT2345";
+studerende1.Fornavn = "Jakob";
+studerende1.Efternavn = "Christensen";
+studerende1.Uddannelse = "Datamatiker";
+
+UdskrivStuderende(studerende1); // udskriv 2
+
+studerende1.ÆndreFornavn("Peter");
+UdskrivStuderende(studerende1); // udskriv 3
+
+
+// Laver et nyt objekt
+Studerende studerende2 = new Studerende("DAT3322", "Peter", "Levinsky", "Datamatiker");
+UdskrivStuderende(studerende2); //udskriv 4
+
+
+
+
+
+
+
+
+
+void UdskrivStuderende(Studerende s)
+{
+    Console.WriteLine($"Id={s.StudieId}, Navn={s.Fornavn} {s.Efternavn} går på uddannelse {s.Uddannelse}");
+}
---
+++ ClassDemoStuderendeKlasse/Studerende.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoStuderendeKlasse
+{
+    public class Studerende
+    {
+        // UML attributter
+        public String StudieId { get; set; } // dette er en property pga. get/set
+        public String Fornavn { get; set; }
+        public String Efternavn { get; set; }
+        public String Uddannelse { get; set; }
+
+        // UML metoder
+
+        public void ÆndreFornavn(String nyNavn)
+        {
+            Fornavn = nyNavn;
+        }
+
+
+        // C# smarte 'metoder'
+        /*
+         * Konstruktører
+         */
+        public Studerende()
+        {
+
+        }
+
+        public Studerende(String Id, String FNavn, String ENavn, String Uddannel)
+        {
+            StudieId = Id;
+            Fornavn = FNavn;    
+            Efternavn = ENavn;
+            Uddannelse = Uddannel;
+        }
+
+    }
+}
